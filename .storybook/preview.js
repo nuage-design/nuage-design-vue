de978177e2bf20a2d8cec4cf21f5458bf5989ecb
Dmitry Shulyak <sokkususu@gmail.com>
2021-04-26 13:27:55 +0300

    storybook

---
+++ .storybook/preview.js
@@ -0,0 +1,11 @@
+import "../src/assets/_fonts.css";
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+};
