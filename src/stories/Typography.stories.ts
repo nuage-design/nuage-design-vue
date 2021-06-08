bfabf202921996a0d4ce9d78a42d2df6195651bc
sokkususu <sokkususu@gmail.com>
2021-06-08 18:24:20 +0300

    :seedling: add typography stories

---
+++ src/stories/Typography.stories.ts
@@ -0,0 +1,16 @@
+export default {
+  title: 'Basic/Typography',
+}
+
+const Template = () => ({
+  template: /* html */ `
+  <h1>Header 1</h1>
+  <h2>Header 2</h2>
+  <h3>Header 3</h3>
+  <h4>Header 4</h4>
+  <h5>Header 5</h5>
+  <h6>Header 6</h6>
+  `,
+})
+
+export const Typography = Template.bind({})
