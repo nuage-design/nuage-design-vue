533d5cef2c81f2a71bca9610829578db9fd36955
Dmitry Shulyak <sokkususu@gmail.com>
2021-11-10 19:04:38 +0300

    add app.vue

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script script setup lang="ts">
+import { NaButton } from './components/na-button/'
+</script>
+
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <NaButton size="default">Кнопка</NaButton>
+</template>
+
+<style></style>
